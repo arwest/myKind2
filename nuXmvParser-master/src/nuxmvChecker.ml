d856392cd3edb5659f854793d81d5feca8d245c6
Andrew <arwest@uiowa.edu>
2019-03-17 11:55:28 -0500

    cleaning up and finishing first pass of semantics checker

--- nuXmvParser-master/src/nuxmvChecker.ml
+++ nuXmvParser-master/src/nuxmvChecker.ml
@@ -40,131 +40,188 @@ let rec s_eval_expr (ltl: bool) (next:bool) (expr: A.nuxmv_expr) : semantic_erro
     | CRange (p, i1, i2) -> if i1 <= i2 then CheckOk else CheckError (p, RangeLowerValue)
     | A.Call (_, ci, nel) -> CheckOk (* TODO: check the next_expr list and the complex identifier *)
     | A.Not (_, e) -> s_eval_expr ltl next e
-    | A.And (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1
-    | A.Or (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1
-    | A.Xor (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1
-    | A.Xnor (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1   
-    | A.Impl (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1
-    | A.Equiv (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1 
-    | A.Eq (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1 
-    | A.NotEq (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1 
-    | A.Lt (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1 
-    | A.Lte (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1
-    | A.Gt (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1 
-    | A.Gte (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1 
-    | A.Plus (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1 
+    | A.And (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                            | CheckOk -> s_eval_expr ltl next e2
+                            | r1 -> r1)
+    | A.Or (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                            | CheckOk -> s_eval_expr ltl next e2
+                            | r1 -> r1)
+    | A.Xor (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                            | CheckOk -> s_eval_expr ltl next e2
+                            | r1 -> r1)
+    | A.Xnor (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                            | CheckOk -> s_eval_expr ltl next e2
+                            | r1 -> r1)   
+    | A.Impl (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                            | CheckOk -> s_eval_expr ltl next e2
+                            | r1 -> r1)
+    | A.Equiv (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                            | CheckOk -> s_eval_expr ltl next e2
+                            | r1 -> r1) 
+    | A.Eq (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                            | CheckOk -> s_eval_expr ltl next e2
+                            | r1 -> r1)
+    | A.NotEq (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                            | CheckOk -> s_eval_expr ltl next e2
+                            | r1 -> r1)
+    | A.Lt (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                            | CheckOk -> s_eval_expr ltl next e2
+                            | r1 -> r1)
+    | A.Lte (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                            | CheckOk -> s_eval_expr ltl next e2
+                            | r1 -> r1)
+    | A.Gt (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                            | CheckOk -> s_eval_expr ltl next e2
+                            | r1 -> r1)
+    | A.Gte (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                            | CheckOk -> s_eval_expr ltl next e2
+                            | r1 -> r1)
+    | A.Plus (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                            | CheckOk -> s_eval_expr ltl next e2
+                            | r1 -> r1)
     | A.Uminus (_, e) -> s_eval_expr ltl next e
-    | A.Minus (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1 
-    | A.Multiply (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1 
-    | A.Divide (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1
-    | A.Mod (_, e1, e2) -> let r1 = s_eval_expr ltl next e1 in 
-                             if r1 == CheckOk then s_eval_expr ltl next e2
-                             else r1 
-    | A.SetExp (_, el) -> let result_list = List.map (s_eval_expr ltl false) el in
-                            if List.for_all (fun x -> x == CheckOk) result_list then CheckOk
-                            else List.find (fun x -> x != CheckOk) result_list
-    | A.CaseExp (_, el) -> let el1 = List.map (fun x -> match x with (x1, _) -> x1) el in
-                           let el2 = List.map (fun x -> match x with (_, x2) -> x2) el in
-                           let result_list1 = List.map (s_eval_expr ltl false) el1 in
-                            if List.for_all (fun x -> x == CheckOk) result_list1 then CheckOk
-                            else let result_list2 = List.map (s_eval_expr ltl false) el2 in
-                                  if List.for_all (fun x -> x == CheckOk) result_list2 then CheckOk
-                                  else List.find (fun x -> x != CheckOk) result_list2
-    | A.NextState (p, e) -> if ltl == false then CheckError (p, LtlUse)
-                            else s_eval_expr ltl next e
-    | A.Globally (p, e) -> if ltl == false then CheckError (p, LtlUse)
-                           else s_eval_expr ltl next e
-    | A.Finally (p, e) -> if ltl == false then CheckError (p, LtlUse)
-                          else s_eval_expr ltl next e
-    | A.Until (p, e1, e2) -> if ltl == false then CheckError (p, LtlUse)
-                             else 
-                                let r1 = s_eval_expr ltl next e1 in 
-                                if r1 == CheckOk then s_eval_expr ltl next e2
-                                else r1 
-    | A.Releases (p, e1, e2) -> if ltl == false then CheckError (p, LtlUse)
-                                else 
-                                    let r1 = s_eval_expr ltl next e1 in 
-                                    if r1 == CheckOk then s_eval_expr ltl next e2
-                                    else r1 
-    | A.PrevState (p, e) -> if ltl == false then CheckError (p, LtlUse)
-                            else s_eval_expr ltl next e
-    | A.NotPrevStateNot (p, e) -> if ltl == false then CheckError (p, LtlUse)
-                                  else s_eval_expr ltl next e
-    | A.Historically (p, e) -> if ltl == false then CheckError (p, LtlUse)
-                               else s_eval_expr ltl next e
-    | A.Once (p, e) -> if ltl == false then CheckError (p, LtlUse)
-                       else s_eval_expr ltl next e
-    | A.Since (p, e1, e2) -> if ltl == false then CheckError (p, LtlUse)
-                             else 
-                                let r1 = s_eval_expr ltl next e1 in 
-                                 if r1 == CheckOk then s_eval_expr ltl next e2
-                                 else r1 
-    | A.Triggered (p, e1, e2) -> if ltl == false then CheckError (p, LtlUse)
-                                 else 
-                                    let r1 = s_eval_expr ltl next e1 in 
-                                     if r1 == CheckOk then s_eval_expr ltl next e2
-                                     else r1
-    | A.NextExp (p, e) -> if next == false then CheckError (p, NextExpr)
+    | A.Minus (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                             | CheckOk -> s_eval_expr ltl next e2
+                             | r1 -> r1)
+    | A.Multiply (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                                | CheckOk -> s_eval_expr ltl next e2
+                                | r1 -> r1)
+    | A.Divide (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                              | CheckOk -> s_eval_expr ltl next e2
+                              | r1 -> r1)
+    | A.Mod (_, e1, e2) -> (match s_eval_expr ltl next e1 with
+                            | CheckOk -> s_eval_expr ltl next e2
+                            | r1 -> r1)
+    | A.SetExp (p, el) ->( if ltl then CheckError (p, LtlUse)
+                           else
+                            let mapped = List.map (s_eval_expr ltl false) el in
+                            let result = List.find_opt (fun x -> x != CheckOk) mapped in
+                            match result with
+                            | Some res -> res
+                            | None -> CheckOk )
+    | A.CaseExp (p, el) -> (if ltl then CheckError (p, LtlUse)
+                            else
+                            let mapped1 = List.map (s_eval_expr ltl false) (List.map fst el) in
+                            let result1 = List.find_opt (fun x -> x != CheckOk) mapped1 in
+                            match result1 with
+                            | Some res -> res
+                            | None -> let mapped2 = List.map (s_eval_expr ltl false) (List.map snd el) in
+                                      let result2 = List.find_opt (fun x -> x != CheckOk) mapped2 in
+                                      match result2 with
+                                      | Some res -> res
+                                      | None -> CheckOk )
+    | A.NextExp (p, e) -> if not next then CheckError (p, NextExpr)
                           else 
-                            match e with 
-                            | A.NextExp _ -> CheckError (p, DoubleNextExpr)
-                            | _ -> s_eval_expr ltl next e
+                            (match e with 
+                             | A.NextExp _ -> CheckError (p, DoubleNextExpr)
+                             | _ -> s_eval_expr ltl next e)
+    | A.NextState (p, e) -> if not ltl then CheckError (p, LtlUse)
+                            else s_eval_expr ltl next e
+    | A.Globally (p, e) -> if not ltl then CheckError (p, LtlUse)
+                           else s_eval_expr ltl next e 
+    | A.Finally (p, e) -> if not ltl  then CheckError (p, LtlUse)
+                          else s_eval_expr ltl next e
+    | A.Until (p, e1, e2) -> (if not ltl then CheckError (p, LtlUse)
+                              else 
+                                match s_eval_expr ltl next e1 with
+                                | CheckOk -> s_eval_expr ltl next e2
+                                | r1 -> r1)
+    | A.Releases (p, e1, e2) -> (if not ltl then CheckError (p, LtlUse)
+                                else 
+                                match s_eval_expr ltl next e1 with
+                                | CheckOk -> s_eval_expr ltl next e2
+                                | r1 -> r1)
+    | A.PrevState (p, e) -> if not ltl then CheckError (p, LtlUse)
+                            else s_eval_expr ltl next e
+    | A.NotPrevStateNot (p, e) -> if not ltl then CheckError (p, LtlUse)
+                                  else s_eval_expr ltl next e
+    | A.Historically (p, e) ->  if not ltl then CheckError (p, LtlUse)
+                                else s_eval_expr ltl next e
+    | A.Once (p, e) -> if not ltl then CheckError (p, LtlUse)
+                       else s_eval_expr ltl next e
+    | A.Since (p, e1, e2) -> if not ltl then CheckError (p, LtlUse)
+                             else
+                                (match s_eval_expr ltl next e1 with
+                                | CheckOk -> s_eval_expr ltl next e2
+                                | r1 -> r1)
+    | A.Triggered (p, e1, e2) -> (if not ltl then CheckError (p, LtlUse)
+                                  else 
+                                 match s_eval_expr ltl next e1 with
+                                 | CheckOk -> s_eval_expr ltl next e2
+                                 | r1 -> r1)
 
 let rec s_eval_expr_type (expr_type: A.expr_type) : semantic_error_type check_result = 
     match expr_type with
-    | A.LtlExpr (_, expr) -> CheckOk (* Check that there is no next () *)
-    | A.NextExpr (_, expr) -> CheckOk (* Check that there is no next ( next (.)) and no ltl*)
-    | A.SimpleExpr (_, expr) -> CheckOk (* Check that there is no next() expr and no ltl *)
+    | A.LtlExpr (_, expr) -> s_eval_expr true false expr
+    | A.NextExpr (_, expr) -> s_eval_expr false true expr
+    | A.SimpleExpr (_, expr) -> s_eval_expr false false expr
     | A.ArrayExpr (_, etl) -> let result_list = List.map s_eval_expr_type etl in
-                                if List.for_all (fun x -> x == CheckOk) result_list then CheckOk
-                                else List.find (fun x -> x != CheckOk) result_list
+                              if List.for_all (fun x -> x == CheckOk) result_list then CheckOk
+                              else List.find (fun x -> x != CheckOk) result_list
+
+let rec s_eval_complex_id (ci: A.comp_ident):  semantic_error_type check_result =
+    match ci with
+    | PerIdent (pos, ci, i) -> s_eval_complex_id ci
+    | BrackIdent (pos, ci, et) -> (match s_eval_complex_id ci with
+                                  | CheckOk -> s_eval_expr_type et
+                                  | other -> other)
+    | _ -> CheckOk
+
+let rec s_eval_state_var_decl (svdl: A.state_var_decl list) :  semantic_error_type check_result =
+    match svdl with
+    | [] -> CheckOk
+    | svd :: t -> match svd with
+                  | ModuleType (p, i, mts) -> 
+                      (match mts with 
+                          | ModuleTypeSpecifier (pos, id, etl) -> 
+                          let mapped = List.map s_eval_expr_type etl in
+                          let result = List.find_opt (fun x -> x != CheckOk) mapped in
+                          match result with
+                          | Some res -> res
+                          | None -> s_eval_state_var_decl t )
+                  | _ -> s_eval_state_var_decl t
+
+let rec s_eval_define_decl (del: A.define_element list):  semantic_error_type check_result =
+    match del with
+    | [] -> CheckOk
+    | svd :: t -> match svd with
+                  | SimpleDef (pos, id, et) -> (match s_eval_expr_type et with
+                                               | CheckOk -> s_eval_define_decl t
+                                               | other -> other)
+                  | ArrayDef (pos, id, et) -> (match s_eval_expr_type et with
+                                               | CheckOk -> s_eval_define_decl t
+                                               | other -> other)
+
+let rec s_eval_assign_const (acl: A.assign_const list):  semantic_error_type check_result =
+    match acl with
+    | [] -> CheckOk
+    | svd :: t -> match svd with
+                  | InitAssign (pos, ci, et) -> (match s_eval_complex_id ci with
+                                                | CheckOk -> s_eval_expr_type et
+                                                | other -> other)
+                  | NextAssign (pos, ci, et) -> (match s_eval_complex_id ci with
+                                                | CheckOk -> s_eval_expr_type et
+                                                | other -> other)
+                  | Assign (pos, ci, et) -> (match s_eval_complex_id ci with
+                                                | CheckOk -> s_eval_expr_type et
+                                                | other -> other)
 
 let s_eval_module_element (me : A.module_element): semantic_error_type check_result = 
     match me with
-    | A.StateVarDecl (_, svdl) -> CheckOk (* Check moduleType specifier param list for expressions to be simple *)
-    | A.DefineDecl (_, del) -> CheckOk (* Check define element expression *)
-    | A.AssignConst (_, acl) -> CheckOk (* Check expr to make sure they are simple, expcept next is next *)
+    | A.StateVarDecl (_, svdl) -> s_eval_state_var_decl svdl
+    | A.DefineDecl (_, del) -> s_eval_define_decl del
+    | A.AssignConst (_, acl) -> s_eval_assign_const acl
     | A.TransConst (_, expr_type) -> s_eval_expr_type expr_type
     | A.LtlSpec (_, expr_type) -> s_eval_expr_type expr_type
 
 let rec semantic_eval (ml:A.nuxmv_module list) : semantic_error_type check_result = 
     match ml with
     | [] -> CheckOk
-    | A.CustomModule (_, _, mel) :: [] -> let result_list = List.map s_eval_module_element mel in
-                                          if List.for_all (fun x -> x == CheckOk) result_list then CheckOk
-                                          else List.find (fun x -> x != CheckOk) result_list
-    | A.CustomModule (_, _, mel) :: t -> let result_list = List.map s_eval_module_element mel in
-                                          if List.for_all (fun x -> x == CheckOk) result_list then semantic_eval t
-                                          else List.find (fun x -> x != CheckOk) result_list
+    | A.CustomModule (_, _, mel) :: [] -> 
+            (let result_list = List.map s_eval_module_element mel in
+            if List.for_all (fun x -> x == CheckOk) result_list then CheckOk
+            else List.find (fun x -> x != CheckOk) result_list)
+    | A.CustomModule (_, _, mel) :: t -> 
+            (let result_list = List.map s_eval_module_element mel in
+            if List.for_all (fun x -> x == CheckOk) result_list then semantic_eval t
+            else List.find (fun x -> x != CheckOk) result_list)
