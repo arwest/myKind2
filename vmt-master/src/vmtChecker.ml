f4be92fab6a1ea004d0a5371057c5b236062ba23
Andrew <arwest@uiowa.edu>
2019-07-09 18:14:38 +0100

    Update vmtChecker.ml
    - adding new missing attribute error
    
    - fixing eval sort to actually return the type (was only returning bool)
    
    - adding new logic to check the full attribute list and update the environment if the next attribute is being used on an ident that doesn't exist already
    
    - updated all functions that are in the line of using the eval_attribute because we need to pass the possible environment change up through the levels
    
    - added to_real operator to the oepration list

--- vmt-master/src/vmtChecker.ml
+++ vmt-master/src/vmtChecker.ml
@@ -15,14 +15,15 @@ module A = VmtAst
 
 type vmt_error = 
     | IdentifierAlreadyExists of Position.t * string
+    | InvalidArgCount of Position.t * int * int
     | InvalidOperator of Position.t * string
     | InvalidType of Position.t * string
     | InvalidTypeWithOperator of Position.t * string * string
-    | MissingIdentifer of Position.t * string
+    | MissingAttribute of Position.t
+    | MissingIdentifier of Position.t * string
     | MissingTerm of Position.t 
     | NonMatchingTypes of Position.t * string * string
     | NotSupported of Position.t * string
-    | InvalidArgCount of Position.t * int * int
 
 let filter_map (f : ('a -> 'b option)) (l : 'a list) : 'b list =
     l |> List.map f 
@@ -38,8 +39,8 @@ let rec eval_sort sort =
     | A.Sort (pos, str) ->(
         match str with
         | "Bool" -> Ok "Bool"
-        | "Int" -> Ok "Bool"
-        | "Real" -> Ok "Bool"
+        | "Int" -> Ok "Int"
+        | "Real" -> Ok "Real"
         | _ -> Error ( InvalidType (pos, str) )
     )
     | A.MultiSort (pos, str, sort_list) -> Error (NotSupported (pos, "MutliSort")) (* TODO: Not currently supported becasue don't know how to handle it *)
@@ -68,98 +69,136 @@ let rec eval_sorted_var_list sorted_var_list local_env =
         )
     )
 
+let rec check_attribute_list attribute_list term_type env pos: ((string * (string * string) list), vmt_error) result =
+    match attribute_list with
+    | [] -> Error (MissingAttribute pos)
+    | attribute :: [] -> (
+        match attribute with
+        | A.NextName (n_pos, n_ident)-> (
+            match filter_map (fun x -> if fst x = n_ident then Some (snd x) else None) env with
+            | [] -> (
+                let env' = (n_ident, term_type) :: env in
+                Ok (term_type, env')
+            )
+            | n_type :: _ -> if n_type = term_type then Ok (n_type, env) else Error (InvalidType (n_pos, n_type))
+        )
+        | A.InitTrue p -> Ok (term_type, env)
+        | A.TransTrue p -> Ok (term_type, env)
+        | A.InvarProperty (p, int) -> Ok (term_type, env)
+        | A.LiveProperty (p, int) -> Error (NotSupported (p, "LiveProperty"))
+    )
+    | attribute :: tail -> (
+        match attribute with
+        | A.NextName (n_pos, n_ident)-> (
+            match filter_map (fun x -> if fst x = n_ident then Some (snd x) else None) env with
+            | [] -> (
+                let env' = (n_ident, term_type) :: env in
+                Ok (term_type, env')
+            )
+            | n_type :: _ -> (
+                if n_type = term_type 
+                    then check_attribute_list tail term_type env pos
+                    else Error (InvalidType (n_pos, n_type))
+            )
+        )   
+        | A.InitTrue p -> check_attribute_list tail term_type env pos
+        | A.TransTrue p -> check_attribute_list tail term_type env pos 
+        | A.InvarProperty (p, int) -> check_attribute_list tail term_type env pos
+        | A.LiveProperty (p, int) -> Error (NotSupported (p, "LiveProperty"))
+    )
+
 let rec eval_term term env =
     match term with
     | A.Ident (pos, ident) -> (
         match filter_map (fun x -> if fst x = ident then Some (snd x) else None) env with
-        | [] -> Error (MissingIdentifer (pos, ident))
-        | var_type :: _ -> Ok var_type
+        | [] -> Error (MissingIdentifier (pos, ident))
+        | var_type :: _ -> Ok (var_type, env)
     )
-    | A.Integer (pos, int) -> Ok "Int"
-    | A.Real (pos, float) -> Ok "Real"
-    | A.True (pos) -> Ok "Bool"
-    | A.False (pos) -> Ok "Bool"
-    | A.Operation (pos, op, term_list) -> eval_operation pos op term_list env
-    | A.AttributeTerm (pos, term, attribute) -> (
-        match (attribute, eval_term term env) with
-        | (NextName (n_pos, n_ident), term_res)-> (
-            match filter_map (fun x -> if fst x = n_ident then Some (snd x) else None) env with
-            | [] -> Error (MissingIdentifer (pos, n_ident))
-            | n_type :: _ -> (
-                match term_res with
-                | Ok term_type -> if n_type = term_type then Ok n_type else Error (InvalidType (n_pos, n_type))
-                | error -> error
-            )
+    | A.Integer (pos, int) -> Ok ("Int", env)
+    | A.Real (pos, float) -> Ok ("Real", env)
+    | A.True (pos) -> Ok ("Bool", env)
+    | A.False (pos) -> Ok ("Bool", env)
+    | A.Operation (pos, op, term_list) -> (
+        match eval_operation pos op term_list env with
+        | Ok (res_type, env') -> Ok (res_type, env')
+        | Error error -> Error error
+    )
+    | A.AttributeTerm (pos, term, attribute_list) -> (
+        match eval_term term env with
+        | Ok (term_type, _) -> (
+            match check_attribute_list attribute_list term_type env pos with
+            | Ok (res_type, env') -> Ok (res_type, env')
+            | Error error -> Error error
         )
-        | (InitTrue p, term_res) -> term_res
-        | (TransTrue p, term_res) -> term_res
-        | (InvarProperty (p, int), term_res) -> term_res
-        | (LiveProperty (p, int), term_res) -> Error (NotSupported (p, "LiveProperty"))
+        | error -> error
     )
 
 and eval_operation pos op term_list env = 
     match (op, eval_term_list term_list env pos) with
-    | ("not", Ok "Bool") -> (
+    | ("not", Ok ("Bool", env')) -> (
         let len = List.length term_list in
         if len > 1 
             then Error (InvalidArgCount (pos, 1, len))
-            else Ok "Bool"
+            else Ok ("Bool", env')
     )
-    | ("not", Ok _type) -> Error (InvalidTypeWithOperator (pos, _type, op))
-    | ("or", Ok "Bool") -> Ok "Bool"
-    | ("or", Ok _type) -> Error (InvalidTypeWithOperator (pos, _type, op))
-    | ("and", Ok "Bool") -> Ok "Bool"
-    | ("and", Ok _type) -> Error (InvalidTypeWithOperator (pos, _type, op))
-    | ("xor", Ok "Bool") -> Ok "Bool"
-    | ("xor", Ok _type) -> Error (InvalidTypeWithOperator (pos, _type, op))
-    | ("=", Ok _type) when _type = "Bool" -> Ok _type
-    | ("=", Ok _type) when _type = "Int" -> Ok "Bool"
-    | ("=", Ok _type) -> Error (InvalidTypeWithOperator (pos, _type, op))
-    | ("<=", Ok _type) when _type = "Int" -> Ok "Bool"
-    | ("<=", Ok _type) when _type = "Real" -> Ok "Bool"
-    | ("<=", Ok _type) -> Error (InvalidTypeWithOperator (pos, _type, op))
-    | ("<", Ok _type) when _type = "Int" -> Ok "Bool"
-    | ("<", Ok _type) when _type = "Real" -> Ok "Bool"
-    | ("<", Ok _type) -> Error (InvalidTypeWithOperator (pos, _type, op))
-    | (">=", Ok _type) when _type = "Int" -> Ok "Bool"
-    | (">=", Ok _type) when _type = "Real" -> Ok "Bool"
-    | (">=", Ok _type) -> Error (InvalidTypeWithOperator (pos, _type, op))
-    | (">", Ok _type) when _type = "Int" -> Ok "Bool"
-    | (">", Ok _type) when _type = "Real" -> Ok "Bool"
-    | (">", Ok _type) -> Error (InvalidTypeWithOperator (pos, _type, op))
-    | ("-", Ok _type) when _type = "Int" -> Ok _type 
-    | ("-", Ok _type) when _type = "Real" -> Ok _type
-    | ("-", Ok _type) -> Error (InvalidTypeWithOperator (pos, _type, op))
-    | ("+", Ok _type) when _type = "Int" -> Ok _type 
-    | ("+", Ok _type) when _type = "Real" -> Ok _type 
-    | ("+", Ok _type) -> Error (InvalidTypeWithOperator (pos, _type, op))
-    | ("*", Ok _type) when _type = "Int" -> Ok _type 
-    | ("*", Ok _type) when _type = "Real" -> Ok _type
-    | ("*", Ok _type) -> Error (InvalidTypeWithOperator (pos, _type, op)) 
-    | ("/", Ok _type) when _type = "Int" -> Ok "Real"
-    | ("/", Ok _type) when _type = "Real" -> Ok _type
-    | ("/", Ok _type) -> Error (InvalidTypeWithOperator (pos, _type, op)) 
-    | ("//", Ok _type) when _type = "Int" -> Ok _type 
-    | ("//", Ok _type) when _type = "Real" -> Ok "Int"
-    | ("//", Ok _type) -> Error (InvalidTypeWithOperator (pos, _type, op)) 
-    | ("mod", Ok _type) when _type = "Int" -> (
+    | ("not",Ok (_type, _)) -> Error (InvalidTypeWithOperator (pos, _type, op))
+    | ("or", Ok ("Bool", env')) -> Ok ("Bool", env')
+    | ("or", Ok (_type, _)) -> Error (InvalidTypeWithOperator (pos, _type, op))
+    | ("and", Ok ("Bool", env')) -> Ok ("Bool", env')
+    | ("and", Ok (_type, _)) -> Error (InvalidTypeWithOperator (pos, _type, op))
+    | ("xor", Ok ("Bool", env')) -> Ok ("Bool", env')
+    | ("xor", Ok (_type, _)) -> Error (InvalidTypeWithOperator (pos, _type, op))
+    | ("to_real", Ok (_type, env')) when _type = "Int" -> Ok ("Real", env')
+    | ("to_real", Ok (_type, env')) when _type = "Real" -> Ok ("Real", env')
+    | ("to_real", Ok (_type, _)) -> Error (InvalidTypeWithOperator (pos, _type, op))
+    | ("=", Ok (_type, env')) when _type = "Bool" -> Ok (_type, env')
+    | ("=", Ok (_type, env')) when _type = "Int" -> Ok ("Bool", env')
+    | ("=", Ok (_type, _)) -> Error (InvalidTypeWithOperator (pos, _type, op))
+    | ("<=", Ok (_type, env')) when _type = "Int" -> Ok ("Bool", env')
+    | ("<=", Ok (_type, env')) when _type = "Real" -> Ok ("Bool", env')
+    | ("<=", Ok (_type, _)) -> Error (InvalidTypeWithOperator (pos, _type, op))
+    | ("<", Ok (_type, env')) when _type = "Int" -> Ok ("Bool", env')
+    | ("<", Ok (_type, env')) when _type = "Real" -> Ok ("Bool", env')
+    | ("<", Ok (_type, _)) -> Error (InvalidTypeWithOperator (pos, _type, op))
+    | (">=", Ok (_type, env')) when _type = "Int" -> Ok ("Bool", env')
+    | (">=", Ok (_type, env')) when _type = "Real" -> Ok ("Bool", env')
+    | (">=", Ok (_type, _)) -> Error (InvalidTypeWithOperator (pos, _type, op))
+    | (">", Ok (_type, env')) when _type = "Int" -> Ok ("Bool", env')
+    | (">", Ok (_type, env')) when _type = "Real" -> Ok ("Bool", env')
+    | (">", Ok (_type, _)) -> Error (InvalidTypeWithOperator (pos, _type, op))
+    | ("-", Ok (_type, env')) when _type = "Int" -> Ok (_type, env')
+    | ("-", Ok (_type, env')) when _type = "Real" -> Ok (_type, env')
+    | ("-", Ok (_type, _)) -> Error (InvalidTypeWithOperator (pos, _type, op))
+    | ("+", Ok (_type, env')) when _type = "Int" -> Ok (_type, env')
+    | ("+", Ok (_type, env')) when _type = "Real" -> Ok (_type, env')
+    | ("+", Ok (_type, _)) -> Error (InvalidTypeWithOperator (pos, _type, op))
+    | ("*", Ok (_type, env')) when _type = "Int" -> Ok (_type, env')
+    | ("*", Ok (_type, env')) when _type = "Real" -> Ok (_type, env')
+    | ("*", Ok (_type, _)) -> Error (InvalidTypeWithOperator (pos, _type, op)) 
+    | ("/", Ok (_type, env')) when _type = "Int" -> Ok ("Real", env')
+    | ("/", Ok (_type, env')) when _type = "Real" -> Ok (_type, env')
+    | ("/", Ok (_type, _)) -> Error (InvalidTypeWithOperator (pos, _type, op)) 
+    | ("//", Ok (_type, env')) when _type = "Int" -> Ok (_type, env')
+    | ("//", Ok (_type, env')) when _type = "Real" -> Ok ("Int", env')
+    | ("//", Ok (_type, _)) -> Error (InvalidTypeWithOperator (pos, _type, op)) 
+    | ("mod", Ok (_type, env')) when _type = "Int" -> (
         let len = List.length term_list in
         if len <> 2 
             then Error (InvalidArgCount (pos, 2, len))
-            else Ok "Int"
+            else Ok ("Int", env')
     )
-    | ("mod", Ok _type) -> Error (InvalidTypeWithOperator (pos, _type, op))
-    | ("abs", Ok _type) -> (
+    | ("mod", Ok (_type, _)) -> Error (InvalidTypeWithOperator (pos, _type, op))
+    | ("abs", Ok (_type, env')) -> (
         let len = List.length term_list in
         if len <> 2 
             then Error (InvalidArgCount (pos, 2, len))
             else 
                 match _type with
-                | "Int" -> Ok _type
-                | "Real" -> Ok _type
+                | "Int" -> Ok ("Int", env')
+                | "Real" -> Ok (_type, env')
                 | _ -> Error (InvalidTypeWithOperator (pos, _type, "abs"))
     )
-    | (op, Ok _type) -> Error (InvalidOperator (pos, op))
+    | (op, Ok (_type, _)) -> Error (InvalidOperator (pos, op))
     | (_, Error error) -> Error error (* TODO: write the logic for dealing with operators and the type given to them *)
 
 
@@ -173,9 +212,9 @@ and eval_term_list term_list env pos =
         match (term_result, tail_result) with
         | (Error error, _) -> Error error
         | (_, Error error) -> Error error
-        | (Ok term_type, Ok list_type) -> (
+        | (Ok (term_type, _), Ok (list_type, _)) -> (
             if term_type = list_type 
-                then Ok term_type
+                then Ok (term_type, env)
                 else Error (NonMatchingTypes (pos, term_type, list_type))
         )
     )
@@ -198,7 +237,7 @@ let eval_expr expr env =
         | (_, _, Error error) -> Error error
         | (_, Ok local_env, Ok return_type) -> (
             match eval_term term local_env with
-            | Ok return_type -> Ok ((ident, return_type) :: env)
+            | Ok (return_type, env') -> Ok ((ident, return_type) :: env')
             | Error error -> Error error
         )
     )
@@ -209,8 +248,8 @@ let eval_expr expr env =
     | A.SetOption (pos, ident, att) -> Error (NotSupported (pos, "SetOption")) 
     | A.Assert (pos, term) -> (
         match eval_term term env with
-        | Ok "Bool" -> Ok env
-        | Ok wrong_type -> Error (InvalidType (pos, wrong_type))
+        | Ok ("Bool", env') -> Ok env'
+        | Ok (wrong_type, _) -> Error (InvalidType (pos, wrong_type))
         | Error error -> Error error
     )
 
